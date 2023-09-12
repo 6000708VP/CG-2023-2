8d3006b43f66fb77414720c4ded8ca36e55422c7
6000708VP <141768751+6000708VP@users.noreply.github.com>
2023-09-12 15:30:29 -0500

    Se esta intentando cambiar de cámaras mediante botones

--- c02/threejs_ej02/script.js
+++ c02/threejs_ej02/script.js
@@ -1,14 +1,19 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-//const camera = new THREE.OrthographicCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
-scene.add(camera);
+function Ortogonal(){
+	const camera = new THREE.OrthographicCamera(5, -5,5, -5, 3, 10 );
+	scene.add(camera);
+}
+function Perspectiva(){
+	const camera1 = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	scene.add(camera)
+}
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const geometry2 = new THREE.SphereGeometry( 1, 10, 10 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ffff} );
-const material1 = new THREE.MeshBasicMaterial( { color: 0xffffff} );
+const material = new THREE.MeshNormalMaterial( { color: 0x00ffff} );
+const material1 = new THREE.MeshNormalMaterial( { color: 0xffffff} );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 camera.position.z = 5;
@@ -19,12 +24,12 @@ sphere.position.x = -2;
 
 
 function animate() {
-    requestAnimationFrame( animate )
-    cube.rotation.x += 0.01; 
-    cube.rotation.y += 0.01;
+    //requestAnimationFrame( animate )
+    //cube.rotation.x += 0.01; 
+    //cube.rotation.y += 0.01;
 	
-	sphere.rotation.x += 0.01; 
-    sphere.rotation.y += 0.01;
+	//sphere.rotation.x += 0.01; 
+    //sphere.rotation.y += 0.01;
 
     renderer.render( scene, camera ); 
 }
