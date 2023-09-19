ab6d819bd4311a72fbb1da6d20eb19989dbb74ae
6000708VP <141768751+6000708VP@users.noreply.github.com>
2023-09-19 15:36:42 -0500

    Add files via upload

--- c02/silla/silla.js
+++ c02/silla/silla.js
@@ -1,24 +1,53 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-const geometry1 = new THREE.CylinderGeometry( 1, 1 );
-const material = new THREE.MeshNormalMaterial( { color: 0x00ffff} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-camera.position.z = 5;
-cube.position.x = -2;
-cube.position.y = -2;
-const cylinder = new THREE.Mesh(geometry1,material);
-scene.add(cylinder);
-cylinder.position.y = 1;
-cylinder.position.x = 0;
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+scene.add(camera);
+// Crea el asiento de la silla (un cubo)
+const seatGeometry = new THREE.BoxGeometry(4, 0.2, 4); // Ancho, alto y profundidad del asiento
+const seatMaterial = new THREE.MeshNormalMaterial({ color: 0x00ffff });
+const seat = new THREE.Mesh(seatGeometry, seatMaterial);
+seat.position.set(0, 2, 0); // Coloca el asiento sobre el suelo
+scene.add(seat);
 
-function rend() {
-    renderer.render( scene, camera ); 
+// Crea el respaldo de la silla (un cilindro)
+const backrestGeometry = new THREE.CylinderGeometry(1, 1, 10, 40); // Radio y altura del respaldo
+const backrestMaterial = new THREE.MeshNormalMaterial({ color: 0xffffff });
+const backrest = new THREE.Mesh(backrestGeometry, backrestMaterial);
+backrest.rotation.x = Math.PI / 2; // Orienta el respaldo en el plano x/y
+backrest.position.set(0, 3, -1.8); // Coloca el respaldo detrás del asiento
+scene.add(backrest);
+
+// Agrega patas de la silla (cuatro cilindros)
+const legGeometry = new THREE.CylinderGeometry(0.1, 0.1, 2, 22); // Radio y altura de las patas
+const legMaterial = new THREE.MeshNormalMaterial({ color: 0x00ff00 });
+
+const Leg1 = new THREE.Mesh(legGeometry, legMaterial);
+Leg1.position.set(-1.5, 1, -1.5); // pata delantera izquierda
+scene.add(Leg1);
+
+const Leg2 = new THREE.Mesh(legGeometry, legMaterial);
+Leg2.position.set(1.5, 1, -1.5); //  pata delantera derecha
+scene.add(Leg2);
+
+const Leg3 = new THREE.Mesh(legGeometry, legMaterial);
+Leg3.position.set(-1.5, 1, 1.5); //  pata trasera izquierda
+scene.add(Leg3);
+
+const Leg4 = new THREE.Mesh(legGeometry, legMaterial);
+Leg4.position.set(1.5, 1, 1.5); //  pata trasera derecha
+scene.add(Leg4);
+
+camera.position.z = 7;
+camera.position.y = 2;
+camera.rotation.z=1;
+
+
+function animate() {
+    requestAnimationFrame(animate);
+ 
+    renderer.render(scene, camera);
 }
 
-rend(); 
+animate();
