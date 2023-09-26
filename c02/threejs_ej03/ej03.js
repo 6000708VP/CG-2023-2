dc223ef4da91afad9627355eb490dfa6efe32169
6000708VP <141768751+6000708VP@users.noreply.github.com>
2023-09-26 15:42:43 -0500

    Add files via upload

--- c02/threejs_ej03/ej03.js
+++ c02/threejs_ej03/ej03.js
@@ -2,26 +2,52 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);   
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.antialias = true;
 document.body.appendChild(renderer.domElement);
+
 const geometry = new THREE.Geometry();
-geometry.vertices.push(
-   new THREE.Vector3(-1, 1, 1),
-);
-geometry.faces.push(
-   new THREE.Face3(0, 1, 5),
-   new THREE.Face3(0, 5, 4),
-   new THREE.Face3(0, 4, 3),
-   new THREE.Face3(0, 2, 1)
-);
+const radio = 3;
+const altura = 5;
+const segmentos = 100;
+const tipVertex = new THREE.Vector3(0, altura, 0);
+
+for(let i=0; i <= segmentos; i++){
+    const theta = (i / segmentos) *Math.PI * 2;
+    const x = radio * Math.cos(theta)
+    const z = radio * Math.sin(theta)
+    const vertex = new THREE.Vector3(x, 0, z)
+    geometry.vertices.push(vertex)
+
+    if (i < segmentos){
+        geometry.faces.push(new THREE.Face3(i, (i+1) % segmentos, segmentos))
+        geometry.faces.push(new THREE.Face3(i, segmentos, segmentos + 1))
+    }
+}
+geometry.vertices.push(new THREE.Vector3(0, altura, 0));
+
 geometry.computeFaceNormals();
-const material = new THREE.MeshNormalMaterial({ color: 0x00ff00,wireframe: true });
+			
+
+
+const TextureLoader = new THREE.TextureLoader();
+const texture = TextureLoader.load("textura.jpg");
+
+const material = new THREE.MeshNormalMaterial({map: texture, wireframe: false , side : THREE.DoubleSide });
+
+
 const customMesh = new THREE.Mesh(geometry, material);
 scene.add(customMesh);
-camera.position.z = 5;
-function animate() {
- requestAnimationFrame(animate);
- renderer.render(scene, camera);
+
+camera.position.z = 10;
+
+function animate(){
+        customMesh.rotation.x += 0.01;
+        customMesh.rotation.y += 0.01;
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
 }
+
 animate();
+
 
 
